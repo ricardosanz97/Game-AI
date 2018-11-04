f7e13aecb71f27cfa39cf058d60568001298dba7
oskarter9 <oscar5teruel@gmail.com>
2018-11-05 00:02:26 +0100

    animacion golpe chaser

---
+++ AI-Game/Assets/Scripts/AssassinAnimationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssassinAnimationController : MonoBehaviour {
+
+    private AssassinNPC _assassinRef;
+	// Use this for initialization
+	void Start () {
+
+        _assassinRef = GetComponent<AssassinNPC>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
